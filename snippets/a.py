5a386fe39e671cf8017c2f7cd076e58022fdb9a4
BuddyBob <thavasantonio@gmail.com>
2021-06-25 15:08:03 -0700

    initial commit

--- snippets/a.py
+++ snippets/a.py
@@ -1,7 +1,7 @@
 import json
 file = open('/Users/aspera/Documents/vscode-ext/CSG/snippets/snippets.code-snippets','w')
 dictionary = {}
-for argCount in range(0,20):
+for argCount in range(1,20):
     dictionary[f"Class {argCount} args"] = {"prefix":f"csg_a{argCount}","description":f"Class template for {argCount} arguments"}
     classHead = "class ${1:Name}:"
     vars = ', '.join(["${"+str(i+2)+":arg"+str(i+1)+"}" for i in range(argCount)])
@@ -10,8 +10,21 @@ for argCount in range(0,20):
     dictionary[f"Class {argCount} args"]["body"] = [classHead,initializer]
     for x in attrs:
         dictionary[f"Class {argCount} args"]["body"].append(x)
-for funcCount in range(0,20):
-    for argCount in range(0,30):
+for funcCount in range(1,20):
+    #title
+    title = f"Class {funcCount} functions"
+    #title, prefix description added
+    dictionary[title] = {"prefix":f"csg_f{funcCount}","description":f"Class template for {funcCount} functions"}
+    #class head
+    classHead = "class ${1:Name}:"
+    dictionary[title]["body"] = [classHead]
+    #function heads added
+    functionHeads = ["\tdef ${"+str(i+2)+":func"+str(i+2)+"}(self):"for i in range(funcCount)]
+    for head in functionHeads:
+        dictionary[title]["body"].append(head)
+        dictionary[title]["body"].append("\t\tpass")
+for funcCount in range(1,20):
+    for argCount in range(1,30):
         #title
         title = f"Class {argCount} args {funcCount} functions"
         #set prefix and description
@@ -26,7 +39,7 @@ for funcCount in range(0,20):
         dictionary[title]["body"] = [classHead,initializer]
         for x in attrs:
             dictionary[title]["body"].append(x)
-        functionHeads = ["\tdef ${"+str(argCount+i+2)+":func"+str(i+1)+"}():"for i in range(funcCount)]
+        functionHeads = ["\tdef ${"+str(argCount+i+2)+":func"+str(i+1)+"}(self):"for i in range(funcCount)]
         for head in functionHeads:
             dictionary[title]["body"].append(head)
             dictionary[title]["body"].append("\t\tpass")
