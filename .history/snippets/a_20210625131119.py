c00d739ab31401cea9148c505b1621c39cb13253
BuddyBob <thavasantonio@gmail.com>
2021-06-25 13:59:42 -0700

    initial commit

---
+++ .history/snippets/a_20210625131119.py
@@ -0,0 +1,34 @@
+import json
+file = open('/Users/aspera/Documents/vscode-ext/CSG/snippets/snippets.code-snippets','w')
+dictionary = {}
+for argCount in range(1,10):
+    dictionary[f"Class {argCount} args"] = {"prefix":f"csg_a{argCount}","description":f"Class template for {argCount} arguments"}
+    classHead = "class ${1:Name}:"
+    vars = ', '.join(["${"+str(i+2)+":arg"+str(i+1)+"}" for i in range(argCount)])
+    initializer = f"\tdef __init__(self,{vars}):"
+    attrs = [f"\t\tself.${i+2} = ${i+2}" for i in range(argCount)]
+    dictionary[f"Class {argCount} args"]["body"] = [classHead,initializer]
+    for x in attrs:
+        dictionary[f"Class {argCount} args"]["body"].append(x)
+for funcCount in range(1,50):
+    for argCount in range(1,50):
+        #title
+        title = f"Class {argCount} args {funcCount} functions"
+        #set prefix and description
+        dictionary[title] = {"prefix":f"csg_a{argCount}_f{funcCount}","description":f"Class template for {argCount} arguments & {funcCount} functions"}
+        #class header
+        classHead = "class ${1:Name}:"
+        #attrivutes
+        vars = ', '.join(["${"+str(i+2)+":arg"+str(i+1)+"}" for i in range(argCount)])
+        initializer = f"\tdef __init__(self,{vars}):"
+        attrs = [f"\t\tself.${i+2} = ${i+2}" for i in range(argCount)]
+        #add body to body key
+        dictionary[title]["body"] = [classHead,initializer]
+        for x in attrs:
+            dictionary[title]["body"].append(x)
+        functionHeads = ["\tdef ${"+str(argCount+i+2)+":func"+str(i)+"}():"for i in range(funcCount)]
+        for head in functionHeads:
+            dictionary[title]["body"].append(head)
+            dictionary[title]["body"].append("\t\tpass")
+    print(dictionary)
+json.dump(dictionary,file,indent=4)
---
+++ README.md
@@ -0,0 +1,20 @@
+# CSG README
+
+I can almost gaurantee that you have created a class in your life. And if that happens to be true, I **know** that you dropped a sweat setting up the base of that class. 
+```
+class Name:
+    def __init__(self,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9...):
+        self.arg1 = arg1
+        ...hundred years later...
+```
+Did you know that it was possible to go a day without sweating? Me neither... until I created the ultimate **CLASS SNIPPETS GENERATOR!**
+
+
+
+## Release Notes
+
+Initial release
+
+### 1.0.0
+
+**Enjoy!**
